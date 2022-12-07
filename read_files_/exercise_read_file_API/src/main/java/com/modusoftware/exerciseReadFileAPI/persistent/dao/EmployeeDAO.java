c7a36830b7c8ba3b62312e5e2d66554ad1ef6863
Daniel Camacho <contactdani.keys@gmail.com>
2022-12-07 12:28:21 -0500

    Update EmployeeDAO.java

--- read_files_/exercise_read_file_API/src/main/java/com/modusoftware/exerciseReadFileAPI/persistent/dao/EmployeeDAO.java
+++ read_files_/exercise_read_file_API/src/main/java/com/modusoftware/exerciseReadFileAPI/persistent/dao/EmployeeDAO.java
@@ -23,51 +23,7 @@ public class EmployeeDAO {
 		
 	
 		
-		
-//
-//		public int getMayor_age() {
-//
-//			DateTimeFormatter formatDateAMD = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-//			LocalDate fecha_nacimiento_format = LocalDate.parse(this.fecha_de_nacimiento, formatDateAMD);
-//			LocalDate fechaActual = LocalDate.now();
-//			Period menores2004 = Period.between(fecha_nacimiento_format, fechaActual);
-//			int años = menores2004.getYears();
-//
-//			return años;
-//
-//		}
-		
-//		public String getfechaDeIngreso() {
-//			
-//			String antiguedadCast = this.antiguedad;
-//			int fecha_ingreso_int = Integer.parseInt(antiguedadCast);
-//			Period meses = Period.ofMonths(fecha_ingreso_int);
-//			LocalDate fechaActual = LocalDate.now();
-//			LocalDate fechaIngreso = fechaActual.minus(meses);
-//			DateTimeFormatter formatDateM = DateTimeFormatter.ofPattern("MM/yyyy");
-//			String fecha_ingreso_format = formatDateM.format(fechaIngreso);
-//			
-//			return fecha_ingreso_format;
-//		}
-		
-//	  public LocalDate getFormatFechaNacimientoAMD() {
-//			
-//			DateTimeFormatter formatDateAMD = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-//			LocalDate date_format_AMD = LocalDate.parse(this.fecha_de_nacimiento, formatDateAMD);
-//			
-//			return date_format_AMD;
-//			
-//		}
-//		public String getFormatFechaNacimientoDMA() {
-//			
-//			DateTimeFormatter formatDateAMD = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-//			DateTimeFormatter formatDateDMA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//			LocalDate date_format_AMD = LocalDate.parse(this.fecha_de_nacimiento, formatDateAMD);
-//			String fechaDeNacimientoDMA = formatDateDMA.format(date_format_AMD);
-//			
-//			return fechaDeNacimientoDMA;
-//			
-//		}
+
 
 
 }
